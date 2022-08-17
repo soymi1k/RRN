6d8f218ca633714c7babf04d7957e806680d3261
soymi1k <soymilkneigh@gmail.com>
2022-08-17 16:20:24 +0900

    personal number check

--- num.js
+++ num.js
@@ -1,28 +1,37 @@
-const num = 6407202771725;
+let str = [];
+let lastNum = [];
 const getValueInText = () => {
+  getState();
+  const mapfn = (arg) => Number(arg);
+  const newArr = Array.from(str, mapfn);
+  lastNum = newArr[12];
+
+  const firstArr = newArr.slice(0, 8);
+  const secondArr = newArr.slice(8, 12);
+
+  const execute = result(firstArr, secondArr);
+
+  if (execute) {
+    alert("유효한 주민번호");
+  } else {
+    alert("유효하지 않은 주민번호");
+  }
+};
+
+const getState = () => {
   const data1 = document.getElementById("firstArr").value;
   const data2 = document.getElementById("secondArr").value;
   let inputData = data1 + data2;
 
-  return inputData;
+  return (str = inputData);
 };
-const mapfn = (arg) => Number(arg);
-const str = String(num);
-const newArr = Array.from(str, mapfn);
-
-const lastNum = newArr[12];
-
-const firstArr = newArr.slice(0, 8);
-const secondArr = newArr.slice(8, 12);
-
 const multiply = (Arr) => {
-  funArr = Arr;
-  const length = funArr.length - 1;
+  const length = Arr.length - 1;
   let start = 2;
   let sum = 0;
 
   for (let i = 0; i <= length; i++) {
-    const no = funArr[i] * start;
+    const no = Arr[i] * start;
     start++;
 
     sum += no;
@@ -33,5 +42,3 @@ const result = (firstArr, secondArr) => {
   const no = (11 - ((multiply(firstArr) + multiply(secondArr)) % 11)) % 10;
   return no == lastNum;
 };
-// console.log((multiply(secondArr) + multiply(firstArr)) % 11);
-// console.log((11 - ((155 + 35) % 11)) % 10);
